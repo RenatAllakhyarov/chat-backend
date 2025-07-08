b13e3f81233db623246fb3a44ac179200d503ca7
RenatAllakhyarov <rembo.909@yandex.ru>
2025-07-08 16:20:55 +0300

    fixed WebSocket connections with proper error handling

--- src/index.ts
+++ src/index.ts
@@ -29,86 +29,91 @@ const messages: Message[] = [];
 wss.on("connection", (ws) => {
     console.log("✅ Новый клиент подключился");
 
-    ws.once("message", (data) => {
-        let parsed;
+    // ws.once("message", (data) => {
+    //     let parsed;
 
+    //     try {
+    //         parsed = JSON.parse(data.toString());
+    //     } catch (err) {
+    //         ws.send(
+    //             JSON.stringify({ type: "error", message: "НЕ ВЕРНЫЙ JSON" })
+    //         );
+    //         ws.close();
+    //     }
+
+    //     if (parsed.type !== "init" || !parsed.username) {
+    //         ws.send(
+    //             JSON.stringify({
+    //                 type: "error",
+    //                 message: "Введи имя свое",
+    //             })
+    //         );
+    //         ws.close();
+    //         return;
+    //     }
+
+    //     const username = parsed?.username;
+
+    //     users.set(ws, username);
+
+    //     console.log(`${username} зашел в чат`);
+
+    //     ws.send(JSON.stringify({ type: "history", messages }));
+
+    ws.on("message", (raw) => {
+        let msg;
         try {
-            parsed = JSON.parse(data.toString());
-        } catch (err) {
-            ws.send(
-                JSON.stringify({ type: "error", message: "НЕ ВЕРНЫЙ JSON" })
-            );
-            ws.close();
-        }
-
-        if (parsed.type !== "init" || !parsed.username) {
-            ws.send(
-                JSON.stringify({
-                    type: "error",
-                    message: "Введи имя свое",
-                })
-            );
-            ws.close();
+            msg = JSON.parse(raw.toString());
+        } catch {
             return;
         }
 
-        const username = parsed?.username;
-
-        users.set(ws, username);
-
-        console.log(`${username} зашел в чат`);
-
-        ws.send(JSON.stringify({ type: "history", messages }));
-
-        ws.on("message", (raw) => {
-            let msg;
-            try {
-                msg = JSON.parse(raw.toString());
-            } catch {
-                return;
-            }
-
-            if (msg.type === "msg" && typeof msg.text === "string") {
-                const username = users.get(ws);
-
-                const message = {
-                    username,
-                    text: msg.text,
-                    timestamp: Date.now(),
-                };
-
-                messages.push(message);
-
-                for (let client of wss.clients) {
-                    if (client.readyState === ws.OPEN) {
-                        client.send(
-                            JSON.stringify({ type: "msg", ...messages })
-                        );
-                    }
-                }
-                console.log(`${username} : ${msg.text} [${message.timestamp}]`);
-            }
-        });
-
-        ws.on("close", () => {
+        if (msg.type === "msg" && typeof msg.text === "string") {
             const username = users.get(ws);
 
-            users.delete(ws);
+            console.log("username: ", username);
 
-            console.log(`❌ Клиент ${username} отключился`);
-            
-        });
+            const message = {
+                username,
+                text: msg.text,
+                timestamp: Date.now(),
+            };
 
-        ws.on("error", (err) => {
-            console.error("⚠️ Ошибка:", err);
-        });
+            messages.push(message);
 
-        app.get("/test", (request: Request, response: Response) => {
-            response.status(200).send("Nigga");
-        });
+            console.log("MESSAGE: ", JSON.stringify(message));
 
-        server.listen(PORT, () => {
-            console.log(`🚀 Сервер слушает http://localhost:${PORT}`);
-        });
+            for (let client of wss.clients) {
+                if (client.readyState === ws.OPEN) {
+                    client.send(JSON.stringify({ type: "msg", ...messages }));
+                }
+            }
+            console.log(`${username} : ${msg.text} [${message.timestamp}]`);
+        }
     });
+
+    //     ws.on("close", () => {
+    //         const username = users.get(ws);
+
+    //         users.delete(ws);
+
+    //         console.log(`❌ Клиент ${username} отключился`);
+
+    //     });
+
+    ws.on("close", (code, reason) => {
+        console.log("🚪 Выход :", code, reason);
+    });
+
+    ws.on("error", (err) => {
+        console.error("⚠️ Ошибка:", err);
+    });
+});
+
+app.get("/test", (request: Request, response: Response) => {
+    response.status(200).send("knigga");
+});
+
+server.listen(PORT, () => {
+    console.log(`🚀 Сервер слушает http://localhost:${PORT}`);
 });
