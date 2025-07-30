61c5c551c97bc2ebb4dfca6da661e538fb32f0b1
Timur Ashkenov <23iktk32@sut.ru>
2025-07-30 22:27:14 +0500

    Deleted log and backend exit from db

--- src/controllers/webSocketController.ts
+++ src/controllers/webSocketController.ts
@@ -2,7 +2,6 @@ import ClientConnectionService from "../services/connection";
 import { WebSocket, WebSocketServer } from "ws";
 
 export function clientConnectionController(ws: WebSocket, wss: WebSocketServer){
-    console.log("Вызван clientConnectionController");
 
     ClientConnectionService.clientConnection(ws,wss);
 }
--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = "mongodb://localhost:27017/practice-chat"; // Измени на свой URL, если нужно
+const MONGO_URL = "mongodb://localhost:27017/practice-chat"; 
 
 export async function connectMongo() {
     try {
         await mongoose.connect(MONGO_URL);
-        console.log("✅ MongoDB подключена");
     } catch (err) {
-        console.error("❌ Ошибка подключения к MongoDB:", err);
         process.exit(1);
     }
 }
